a51d4108d633956255fea617d610c4dfb4404936
yusksto <52541085+yusksto@users.noreply.github.com>
2022-08-15 03:44:26 +0900

    first commit

---
+++ fclayer.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Eigen/Dense"
+
+class fclayer
+{
+public:
+    Eigen::VectorXd calc(Eigen::VectorXd X);
+
+private:
+
+};
+
+fclayer::fclayer()
+{
+}
+
+Eigen::VectorXd fclayer::calc(Eigen::VectorXd X)
+{
+}
